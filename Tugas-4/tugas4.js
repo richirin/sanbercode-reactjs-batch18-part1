609e7659f8ea6ac71374c6c2694e2fc5d4e61f37
richirin <richirichrin@gmail.com>
2020-10-01 12:21:10 +0700

    update tugas 4

--- Tugas-4/tugas4.js
+++ Tugas-4/tugas4.js
@@ -1,11 +1,21 @@
 // Soal 1
+console.log("--------------jawaban soal 1--------------");
 var i = 2;
+console.log("Loop Pertama");
 while (i <= 20) {
   console.log(i + " - I love coding");
   i += 2;
 }
 
+console.log("Loop Kedua");
+
+while (i - 2 >= 2) {
+  i -= 2;
+  console.log(i + " - I love coding");
+}
+
 // soal 2
+console.log("--------------jawaban soal 2--------------");
 for (i = 1; i <= 20; i++) {
   if (i % 3 == 0 && i % 2 != 0) {
     console.log(i + " - I Love Coding");
@@ -17,6 +27,7 @@ for (i = 1; i <= 20; i++) {
 }
 
 // Soal 3
+console.log("--------------jawaban soal 3--------------");
 var pyramid = "";
 for (i = 1; i <= 7; i++) {
   for (j = 1; j <= i; j++) {
@@ -27,11 +38,13 @@ for (i = 1; i <= 7; i++) {
 }
 
 // Soal 4
+console.log("--------------jawaban soal 4--------------");
 var kalimat = "saya sangat senang belajar javascript";
 var arr = kalimat.split(" ");
 console.log(arr);
 
 // Soal 5
+console.log("--------------jawaban soal 5--------------");
 var daftarBuah = [
   "2. Apel",
   "5. Jeruk",
@@ -41,7 +54,9 @@ var daftarBuah = [
 ];
 
 var sortArr = daftarBuah.sort();
-var arrToStr = sortArr.join("\n");
+for (i = 0; i < sortArr.length; i++) {
+  console.log(sortArr[i]);
+}
 
-console.log(arrToStr);
+// console.log(arrToStr);
 // console.log(daftarBuah.sort().join("\n"));
