ce77b0c0a631225bb40b86cce55b46b3a267f6d1
richi <richi.ramadhan@fore.coffee>
2020-10-07 01:10:12 +0700

    tugas 7

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -46,7 +46,22 @@ class Clock {
     this.timer;
   }
   render() {
-    console.log(this._template);
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    var mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    var secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    var output = this._template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+    console.log(output);
   }
   stop() {
     clearInterval(this.timer);
